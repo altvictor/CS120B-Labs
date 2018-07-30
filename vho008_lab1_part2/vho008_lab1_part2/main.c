8d88e9b987e5cf57930aa1a92cca662857fa7f52
altvictor <vho008@ucr.edu>
2018-07-30 15:23:30 -0700

    part 2
    finished part 2 of lab 1

---
+++ vho008_lab1_part2/vho008_lab1_part2/main.c
@@ -0,0 +1,50 @@
+/*
+ * vho008_lab1_part2.c
+ *
+ * Created: 7/30/2018 2:41:53 PM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+
+	unsigned char cntavail = 0x00;
+	unsigned char spot0 = 0x00;
+	unsigned char spot1 = 0x00;
+	unsigned char spot2 = 0x00;
+	unsigned char spot3 = 0x00;
+	
+    while (1) 
+    {
+		//read input
+		spot0 = PINA & 0x01;
+		spot1 = PINA & 0x02;
+		spot2 = PINA & 0x04;
+		spot3 = PINA & 0x08;	
+			
+		//calculate
+		cntavail = 0x04;
+		
+		if (spot0 == 0x01){
+			cntavail--;
+		}
+		if (spot1 == 0x02){
+			cntavail--;
+		}
+		if (spot2 == 0x04){
+			cntavail--;
+		}
+		if (spot3 == 0x08){
+			cntavail--;
+		}
+		
+		//output
+		PORTC = cntavail;
+    }
+}
+
