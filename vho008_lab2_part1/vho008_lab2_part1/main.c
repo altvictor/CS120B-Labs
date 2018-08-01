f403a4e58fab8fe1674abc6eb2a9d8e61b551095
altvictor <vho008@ucr.edu>
2018-08-01 15:26:14 -0700

    Revert "Revert "lab 2 part 3""
    This reverts commit 680864b4d038fb880b2deea346eef192a5c55292.

--- vho008_lab2_part1/vho008_lab2_part1/main.c
+++ vho008_lab2_part1/vho008_lab2_part1/main.c
@@ -1,8 +1,10 @@
 /*
- * vho008_lab2_part1.c
+ * Victor Ho
+ * Luis Enriquez-Contreras
+ * Lab Section: 21
+ * Assignment: Lab 2 Exercise 1
  *
- * Created: 7/31/2018 12:43:29 PM
- * Author : ucrcse
+ * I acknowledge all content contained herein, excluding template or example code, is my own original work.
  */ 
 
 #include <avr/io.h>
@@ -26,8 +28,12 @@ int main(void)
 
 		for (unsigned short i = 0; i < 8; i++){	
 			tmpA = GetBit(PINA, i);
+			tmpB = GetBit(PINB, i);
 			if (tmpA == 0x01){
 				 count++;
+			}
+			if (tmpB == 0x01){
+				count++;
 			}
 		}
 		
