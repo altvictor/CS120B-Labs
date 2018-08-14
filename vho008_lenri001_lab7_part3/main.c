42c5f76c883955df55432083cb57a676e5433b99
lenri001 <lenri001@ucr.edu>
2018-08-14 14:28:38 -0700

    Lab7Part3

--- vho008_lenri001_lab7_part3/main.c
+++ vho008_lenri001_lab7_part3/main.c
@@ -2,27 +2,46 @@
  * Victor Ho
  * Luis Enriquez-Contreras
  * Lab Section: 21
- * Assignment: Lab 7 Exercise 1
+ * Assignment: Lab 7 Exercise 3
  *
  * I acknowledge all content contained herein, excluding template or example code, is my own original work.
  */ 
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include "timer.h"
-#include "taskScheduler.h"
-#include "ThreeLED.h"
-#include "BlinkLED.h"
-#include "CombineLED.h"
 
-typedef struct Task;
+typedef struct Task{
+	int state;
+	unsigned long period;
+	unsigned long elapsedTime;
+	int (*TickFct)(int);
+} task;
 
-const unsigned char tasksSize = 3;
-Task tasks[tasksSize];
-unsigned long tasksPeriod = 1000;
+unsigned long _avr_timer_M = 1;
+unsigned long _avr_timer_cntcurr = 0;
+
+void TimerSet(unsigned long period);
+void TimerOn();
+void TimerISR();
+unsigned long gcd(unsigned long a, unsigned long b);
+
+#define tasksSize 4
+task tasks[tasksSize];
+unsigned long tasksPeriod = 1;
 
 unsigned char outputThree = 0x00;
 unsigned char outputBlink = 0x00;
+unsigned char outputSpeaker = 0x00;
+
+enum ThreeLED_States;
+int ThreeLED_tick(int state);
+enum BlinkLED_States;
+int BlinkLED_tick(int state);
+enum Speaker_States;
+int Speaker_tick(int state);
+enum CombineLED_States;
+int CombineLED_tick(int state);
+
 
 void initializeTasks();
 
@@ -36,28 +55,216 @@ int main()
 	TimerOn();
 		
     while (1) {}
+	return 0;
 }
 
 void initializeTasks(){
 	unsigned char index = 0;
 
 	tasks[index].state = -1;
-	tasks[index].period = tasksPeriod;
+	tasks[index].period = 300;
 	tasks[index].elapsedTime = 0;
 	tasks[index].TickFct = &ThreeLED_tick;
-	tasksPeriod = tasks[index].period;
+//	tasksPeriod = tasks[index].period;
 	index++;
 	
 	tasks[index].state = -1;
-	tasks[index].period = tasksPeriod;
+	tasks[index].period = 1000;
 	tasks[index].elapsedTime = 0;
 	tasks[index].TickFct = &BlinkLED_tick;
-	tasksPeriod = gcd(tasksPeriod, tasks[index].period);
+//	tasksPeriod = gcd(tasksPeriod, tasks[index].period);
 	index++;
 	
 	tasks[index].state = -1;
-	tasks[index].period = tasksPeriod;
+	tasks[index].period = 2;
+	tasks[index].elapsedTime = 0;
+	tasks[index].TickFct = &Speaker_tick;
+	//	tasksPeriod = gcd(tasksPeriod, tasks[index].period);
+	index++;
+	
+	tasks[index].state = -1;
+	tasks[index].period = 100;
 	tasks[index].elapsedTime = 0;
 	tasks[index].TickFct = &CombineLED_tick;
-	tasksPeriod = gcd(tasksPeriod, tasks[index].period);
+//	tasksPeriod = gcd(tasksPeriod, tasks[index].period);
+}
+
+
+void TimerSet(unsigned long period){
+	_avr_timer_M = period;
+	_avr_timer_cntcurr = _avr_timer_M;
+}
+
+void TimerOn(){
+	TCCR1B = 0x0B;
+	OCR1A = 125;
+	TIMSK1 = 0x02;
+	TCNT1 = 0;
+	_avr_timer_cntcurr = _avr_timer_M;
+	SREG |= 0x80;
+}
+
+void TimerISR(){
+	unsigned char i;
+	for (i = 0; i < tasksSize; ++i){
+		if (tasks[i].elapsedTime >= tasks[i].period){
+			tasks[i].state = tasks[i].TickFct(tasks[i].state);
+			tasks[i].elapsedTime = 0;
+		}
+		tasks[i].elapsedTime += tasksPeriod;
+	}
+}
+
+void TimerOff(){
+	TCCR1B = 0x00;
+}
+
+ISR(TIMER1_COMPA_vect){
+	_avr_timer_cntcurr--;
+	if (_avr_timer_cntcurr == 0){
+		TimerISR();
+		_avr_timer_cntcurr = _avr_timer_M;
+	}
+}
+
+unsigned long gcd(unsigned long a, unsigned long b){
+	unsigned long remainder = a % b;
+	if (remainder == 0){
+		return b;
+	}
+	return gcd(b,remainder);
+}
+
+enum ThreeLED_States {Three_start, light0, light1, light2};
+
+int ThreeLED_tick(int state){
+	switch (state){ //transitions
+		case Three_start:
+		state = light0;
+		break;
+		case light0:
+		state = light1;
+		break;
+		case light1:
+		state = light2;
+		break;
+		case light2:
+		state = light0;
+		break;
+		default:
+		state = Three_start;
+		break;
+	}
+	
+	switch (state){ //actions
+		case Three_start:
+		outputThree = 0x00;
+		break;
+		case light0:
+		outputThree = 0x01;
+		break;
+		case light1:
+		outputThree = 0x02;
+		break;
+		case light2:
+		outputThree = 0x04;
+		break;
+		default:
+		break;
+	}
+	return state;
+}
+
+enum BlinkLED_States {Blink_start, lightOn, lightOff};
+
+int BlinkLED_tick(int state){
+	switch (state){ //transitions
+		case Blink_start:
+		state = lightOn;
+		break;
+		case lightOn:
+		state = lightOff;
+		break;
+		case lightOff:
+		state = lightOn;
+		break;
+		default:
+		state = Blink_start;
+		break;
+	}
+	
+	switch (state){ //actions
+		case Blink_start:
+		outputBlink = 0x00;
+		break;
+		case lightOn:
+		outputBlink = 0x01;
+		break;
+		case lightOff:
+		outputBlink = 0x00;
+		break;
+		default:
+		break;
+	}
+	return state;
+}
+
+enum Speaker_States {spkr_Start, spkr_On, spkr_Off};
+int Speaker_tick(int state){
+	switch (state){
+		case spkr_Start:
+		bt_state = spkr_On;
+		break;
+		case spkr_On:
+		bt_state = spkr_Off;
+		break;
+		case spkr_Off:
+		bt_state = spkr_On;
+		break;
+		default:
+		break;
+	}
+	switch(state){
+		case spkr_Start:
+		break;
+		case spkr_On:
+		btVal = 0x10;
+		break;
+		case spkr_Off:
+		btVal = 0x00;
+		break;
+	}
+	
+
+enum CombineLED_States {Combine_start, display};
+	
+
+int CombineLED_tick(int state){
+	unsigned char tmpB = 0x00;
+	
+	switch (state){ //transitions
+		case Combine_start:
+		state = display;
+		break;
+		case display:
+		state = display;
+		break;
+		default:
+		state = Combine_start;
+		break;
+	}
+	
+	switch (state){ //actions
+		case Combine_start:
+		tmpB = 0x00;
+		break;
+		case display:
+		tmpB = outputThree;
+		tmpB += (outputBlink << 3);
+		PORTB = tmpB;
+		break;
+		default:
+		break;
+	}
+	return state;
 }
