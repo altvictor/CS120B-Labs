014ace800facc21451bcb638d2f368a382d8821f
altvictor <vho008@ucr.edu>
2018-08-22 13:51:18 -0700

    created file for custom project

---
+++ vho008_custom_project/vho008_custom_project/main.c
@@ -0,0 +1,15 @@
+/*
+ * Victor Ho
+ * Lab Section: 21
+ * Assignment: Custom Project
+ *
+ * I acknowledge all content contained herein, excluding template or example code, is my own original work.
+ */ 
+#include <avr/io.h>
+
+int main(void)
+{
+	
+    while (1) {}
+}
+
